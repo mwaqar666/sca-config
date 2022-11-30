911ddbc9d0b32180f95fd86e6bdfa8646bf20200
Muhammad Waqar <mwaqar666@yahoo.com>
2022-11-30 16:39:40 +0500

    Updated package.json to add @types/validator as development dependency

--- src/config.module.ts
+++ src/config.module.ts
@@ -1,7 +1,7 @@
 import { NestConfigConfig } from "@/config";
 import { Module } from "@nestjs/common";
 import { ConfigModule as NestConfigModule } from "@nestjs/config";
-import { BaseModule } from "sca-core";
+import { BaseModule } from "sca-utils";
 
 @Module({
 	imports: [NestConfigModule.forRoot(NestConfigConfig)],
